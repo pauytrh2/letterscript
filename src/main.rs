9e2bdd2d2e07163960328f213cdc47e11cfd2d03
pauytrh2 <pauytrh@gmail.com>
2025-07-15 21:44:16 +0300

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = match args.get(1) {
+        Some(path) => path,
+        None => {
+            eprintln!("Error: No file path argument provided.\nPlease provide a file path.");
+            std::process::exit(1);
+        }
+    };
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    dbg!(contents);
+}
