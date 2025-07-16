0bc088faf3579ac6a363d9d7e4cdf104be60b84b
pauytrh2 <pauytrh@gmail.com>
2025-07-16 20:36:57 +0300

    Improve tokenizing so it can actually display the exit code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env, fs, process::Command};
+use std::{env, fs, process::Command};
 
 mod token;
 use token::*;
@@ -36,7 +36,6 @@ fn main() {
 }
 
 fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
-    let keywords = get_keywords();
     let mut tokens = Vec::new();
 
     let mut words = input.split_whitespace().peekable();
@@ -59,41 +58,38 @@ fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
             });
             expect_int = false;
         } else {
-            match core {
-                "Dear" => {
-                    tokens.push(Token {
-                        _type: TokenType::Function,
-                        value: None,
-                    });
-                    expect_string = true;
-                }
-                "Regards" => {
-                    tokens.push(Token {
-                        _type: TokenType::Return,
-                        value: None,
-                    });
+            if core == "Dear" {
+                tokens.push(Token {
+                    _type: TokenType::Function,
+                    value: None,
+                });
+                expect_string = true;
+            } else if core == "Regards" {
+                tokens.push(Token {
+                    _type: TokenType::Return,
+                    value: None,
+                });
 
-                    if let Some(next) = words.peek() {
-                        if next.ends_with(',') {
-                            words.next();
-                            tokens.push(Token {
-                                _type: TokenType::Comma,
-                                value: None,
-                            });
-                            expect_int = true;
-                        }
-                    }
-                }
-                _ => {
-                    if let Some(token_type) = keywords.get(core) {
+                if let Some(next) = words.peek() {
+                    if next.ends_with(',') {
+                        words.next();
                         tokens.push(Token {
-                            _type: token_type.clone(),
-                            value: Some(core),
+                            _type: TokenType::Comma,
+                            value: None,
                         });
-                    } else {
-                        eprintln!("Unknown token: {core}");
+                        expect_int = true;
                     }
                 }
+            } else if core.chars().all(|c| c.is_ascii_digit()) {
+                tokens.push(Token {
+                    _type: TokenType::Int,
+                    value: Some(core),
+                });
+            } else {
+                tokens.push(Token {
+                    _type: TokenType::String,
+                    value: Some(core),
+                });
             }
         }
 
@@ -118,27 +114,42 @@ fn strip_punctuation(word: &str) -> (&str, Option<TokenType>) {
     (word, None)
 }
 
-fn get_keywords() -> HashMap<&'static str, TokenType> {
-    use TokenType::*;
-
-    HashMap::from([
-        ("Dear", Function),
-        ("main", String),
-        ("Regards", Return),
-        ("0", Int),
-    ])
-}
-
 fn to_asm(tokens: Vec<Token>) -> String {
     let mut output = String::from("global _start\n\n_start:\n");
+    let exit_code_line = format!(
+        "    mov edi, {}\n",
+        find_exit_code(tokens.as_slice()).expect("Failed to find exit code")
+    );
 
     for token in tokens {
         if token._type == TokenType::Return {
             output.push_str("    mov eax, 60\n");
-            output.push_str("    mov rdi, 0\n");
+            output.push_str(&exit_code_line);
             output.push_str("    syscall");
         }
     }
 
     output
+}
+
+fn find_exit_code(tokens: &[Token]) -> Option<i32> {
+    for i in 0..tokens.len() {
+        if tokens[i]._type == TokenType::Return {
+            if let Some(Token {
+                _type: TokenType::Comma,
+                ..
+            }) = tokens.get(i + 1)
+            {
+                if let Some(Token {
+                    _type: TokenType::Int,
+                    value: Some(v),
+                }) = tokens.get(i + 2)
+                {
+                    return v.parse().ok();
+                }
+            }
+        }
+    }
+
+    None
 }
